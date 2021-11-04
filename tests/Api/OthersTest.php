274c8dc03b6d104881b4b8254ef151bbb7ccdace
gustavo9601 <tavo9601@gmail.com>
2021-11-03 23:57:58 -0500

    add @anotations to execute only group of test by console

--- tests/Api/OthersTest.php
+++ tests/Api/OthersTest.php
@@ -4,6 +4,23 @@ namespace Tests\Api;
 
 use Tests\TestCase;
 
+
+// Permite colocar agrupaciones a las pruebas de forma que se puedan llamar, desde la consola
+// Bien sea para ejecutar una agrupacion o excluirla
+
+/*
+ * // Ejecutando solo una agrupacion de pruebas
+ * .\vendor\bin\phpunit --group <<pruebasColecciones>>
+ *
+ * // Ejecutando las pruebas excluuendo una agrupacion de pruebas
+ * .\vendor\bin\phpunit --exclude-group <<pruebasColecciones>>
+ * */
+
+/**
+ * @group pruebasColecciones
+ * @group TICKET-123
+ * */
+
 class OthersTest extends TestCase
 {
 
