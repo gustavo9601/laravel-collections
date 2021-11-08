1f1972571ae1ba9a92829d5682c286958fa56e18
LPCGUSTAVOMPR\Administrador <Gustavo Marquez>
2021-11-07 21:31:16 -0500

    add telescope

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class PostController extends Controller
 {
     public function index(){
-        return Post::with(['user'])->idget();
+        return Post::with(['user'])->get();
     }
 }
